6f3d9484dc5c1c1608c4787a1be2e3278ff3613f
bhanubandi1999 <bhanureddybandi@gmail.com>
2024-08-22 12:35:06 +0530

    new

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = ap-south-1
+}
+
+
+
+resource "aws_instance" "first" {
+
+    ami = 
+    instance_type = 
+    key_name = 
+    security_groups = 
+    subnet_id = 
+    
+  
+}
